4d7975fb68de9c6aa1f9f96bb9e3f103924d0167
Wenrui Zhao <129207498+Captainbias@users.noreply.github.com>
2025-06-21 17:47:55 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Initialize camera and MediaPipe
 cap = cv2.VideoCapture(0)
 mp_drawing = mp.solutions.drawing_utils
-mp_pose    = mp.solutions.pose
+mp_pose    = mp.solutions.pose 
 
 def gen_frames():
     with mp_pose.Pose(
@@ -43,7 +43,7 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-@app.route('/video_feed')
+@app.route('/video_feed/')
 def video_feed():
     return Response(
         gen_frames(),
@@ -54,6 +54,7 @@ def video_feed():
 def index():
     # A simple page that shows the video stream
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
